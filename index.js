5858dc5381254c495a7ed40f18b11a8bbdd97bdc
Roxanne Weber <89050736+stackdev-web@users.noreply.github.com>
2021-10-05 18:58:52 -0500

    all tests pass

--- index.js
+++ index.js
@@ -130,32 +130,58 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(age, weight){
+function hungryDog(weight, age){
   if (age >= 1) {
     let feedDog = 0;
     if (weight <= 5) {
-      feedDog = weight * .05;
+    feedDog = weight * .05;
+    return feedDog;
     }
     else if (weight > 5 && weight <= 10) {
-      feedDog = weight * .04;
+    feedDog = weight * .04;
+    return feedDog;
     }
     else if (weight > 10 && weight <= 15) {
-      feedDog = weight * .03;
+    feedDog = weight * .03;
+    return feedDog;
     }
     else if (weight > 15) {
-      feedDog = weight * .02;
-    }
-
+    feedDog = weight * .02;
     return feedDog;
-
+    }
+    console.log(`Your dog will need ${feedDog} pounds of food.`)  
+    
   }
+
   else if (age < 1) {
-
-
-
-
+    let feedPuppy = 0;
+    if (age < .167) {
+      return feedPuppy;
+    }
+    else if (age >= .167 && age <.333) {
+      feedPuppy = (weight * .1);
+      console.log('your puppy is older than .167 and younger than .333');
+      return feedPuppy;
+    }
+    else if (age >= .333 && age < .583) {
+      feedPuppy = (weight * .05);
+      console.log('your puppy is >= .333 or < .583')
+      return feedPuppy;
+    }
+    else if (age >= .583 && age < 1) {
+      feedPuppy = (weight * .04);
+      console.log('your puppy is >= .583 and < 1')
+      return feedPuppy;
+    }
+    else {
+      console.log('Please enter valid data.')
+    }
+    console.log(`Your puppy will need ${feedPuppy} pound(s) of food.`)
   }
+     
 }
+
+hungryDog(10, .2)
 
 
 
@@ -181,8 +207,41 @@ HINT: Remember that the order in which we pass in our arguments matters when it 
 */
 
 function game(user, computer){
-  /*add your code here*/
-}
+ if (user === computer) {
+    return(`it's a tie`);
+  } 
+
+  else if (user === 'rock' && computer === 'scissors') {
+    return ('you win!');
+  }
+
+  else if (user === 'rock' && computer === 'paper') {
+    return ('you lose!');
+    }
+
+  else if (user === 'scissors' && computer === 'rock') {
+    return ('you lose!');
+  }
+
+  else if (user === 'scissors' && computer === 'paper') {
+    return ('you win!');
+  }
+
+  else if (user === 'paper' && computer === 'rock') {
+    return ('you win!');
+  }
+
+  else if (user === 'paper' && computer === 'scissors') {
+    return ('you lose!');
+  }
+
+  else {
+    return ('please enter valid entries');
+  }
+ }
+
+ game('paper', 'paper');
+
 
 
 
@@ -197,9 +256,12 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  let miles = (kilometers * 0.621371);
+  return miles;
 }
+
+miles(10)
 
 
 
@@ -211,10 +273,12 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  let numFeet = (cm / 30.48);
+  return numFeet;
 }
 
+feet(30.48)
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -227,9 +291,14 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(numStart){
+      for (let i = numStart; i !== 0; i--) {
+        return(`${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1} bottles of soda on the wall`);
+      }
 }
+
+annoyingSong(5)
+
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -247,10 +316,28 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if (score >= 90 && score <= 100) {
+    return('you got an A');
+  }
+  else if (score >=80 && score <=89) {
+    return('you got a B');
+  }
+  else if (score >=70 && score <=79) {
+    return('you got a C');
+  }
+  else if (score >=60 && score <= 69) {
+    return('you got a D');
+  }
+  else if (score < 60) {
+    return('you got an F');
+  }
+  else {
+    return('please enter a valid score');
+  }
 }
 
+grade(85)
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
@@ -266,9 +353,21 @@ HINT - try looking up the .includes() method
 */
 
 
-function vowelCounter(/*add your code here*/) {
-  /*add your code here*/
+function vowelCounter(string) {
+  const stringLower = string.toLowerCase();
+  const stringArr = Array.from(string);
+  let vowelCount = 0;
+    for (let i = 0; i < stringArr.length; i++) {
+      if (stringLower[i] === 'a' || stringLower[i] === 'e' || stringLower[i] === 'i' || stringLower[i] === 'o' || stringLower[i] === 'u') {
+        vowelCount = vowelCount + 1;
+      }
+      
+    }
+    console.log(`number of vowels = ${vowelCount}`);
 }
+
+    vowelCounter('This is me')
+      
 
 
 
